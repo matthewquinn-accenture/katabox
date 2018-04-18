63c138864b2db5f783efb8df3b4358bffd6e88db
matthew quinn <matthewquinn@gmail.com>
2018-04-18 20:47:54 +0000

    Refactored fizz conditionals

--- javascript/fizzbuzz/src/fizzbuzz.js
+++ javascript/fizzbuzz/src/fizzbuzz.js
@@ -1,11 +1,15 @@
 export const calculator = (input) => {
   let output;
 
-  if (input % 3 == 0) {
+  if (isFizz(input)) {
     output = "fizz"
   } else {
     output = input
   }
 
   return output
+}
+
+const isFizz = (input) => {
+  return input % 3 == 0
 }
