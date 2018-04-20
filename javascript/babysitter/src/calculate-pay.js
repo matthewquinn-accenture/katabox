1c4e27f0d3747c2de46ca7c21df6aa408d1c468d
matthew quinn <matthewquinn@gmail.com>
2018-04-20 01:01:16 +0000

    Calculates bedtime rates

--- javascript/babysitter/src/calculate-pay.js
+++ javascript/babysitter/src/calculate-pay.js
@@ -1,7 +1,14 @@
 const START_TIME_RATE = 12
+const BEDTIME_RATE = 8
+
+const MIDNIGHT = 12
 
 const calculateStartTime = (startTime, bedTime) => {
   return (bedTime - startTime) * START_TIME_RATE
 }
 
-export { START_TIME_RATE, calculateStartTime }
+const calculateBedtime = (bedtime) => {
+  return (MIDNIGHT - bedtime) * BEDTIME_RATE
+}
+
+export { START_TIME_RATE, calculateStartTime, BEDTIME_RATE, calculateBedtime }
--- javascript/babysitter/test/calculate-pay.spec.js
+++ javascript/babysitter/test/calculate-pay.spec.js
@@ -1,6 +1,6 @@
 import Mocha from 'mocha'
 import { expect } from 'chai'
-import { START_TIME_RATE, calculateStartTime } from '../src/calculate-pay'
+import { START_TIME_RATE, calculateStartTime, BEDTIME_RATE, calculateBedtime } from '../src/calculate-pay'
 
 describe("Canary test", () => {
   it("true equals true", () => {
@@ -13,6 +13,12 @@ describe("Babysitter Kata", () => {
   describe("start-time shift", () => {
     it("Calculate start-time rate", () => {
       expect(START_TIME_RATE * 1).to.equal(calculateStartTime(4, 5));
+    });
+  });
+
+  describe("bedtime shift", () => {
+    it("Calculate bedtime rate", () => {
+      expect(BEDTIME_RATE * 4).to.equal(calculateBedtime(8));
     });
   });
 
