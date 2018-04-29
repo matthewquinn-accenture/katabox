158844d18c6c96b0e6288fbee976a7de2e995d46
matthew quinn <matthewquinn@gmail.com>
2018-04-29 18:29:52 +0000

    Refactored validate-shifts file

--- javascript/babysitter/src/validate-shifts.js
+++ javascript/babysitter/src/validate-shifts.js
@@ -1,17 +1,29 @@
 export const validateStartTime = (startTime) => {
-  if (startTime < 5) {
+  if (isStartTimeBefore5PM(startTime)) {
     throw new Error("Can't start before 5PM!")
   }
 }
 
+const isStartTimeBefore5PM = (startTime) => {
+  return startTime < 5
+}
+
 export const validateBedtime = (bedtime) => {
-  if (bedtime <= 4) {
+  if (isBedtimeAfterMidnight(bedtime)) {
     throw new Error("Can't stay up past MIDNIGHT!")
   }
 }
 
+const isBedtimeAfterMidnight = (bedtime) => {
+  return bedtime <= 4
+}
+
 export const validateEndtime = (endtime) => {
-  if (endtime > 4) {
+  if (isEndtimeAfter4AM(endtime)) {
     throw new Error("Can't work later than 4AM!")
   }
+}
+
+const isEndtimeAfter4AM = (endtime) => {
+  return endtime > 4
 }
