939508bbbf3cdfb5e6a991f8e9a7e39f2adfa851
matthew quinn <matthewquinn@gmail.com>
2018-05-21 23:34:08 +0000

    Refactored pencilDegregation function

--- javascript/pencil-durability/src/pencil.js
+++ javascript/pencil-durability/src/pencil.js
@@ -14,7 +14,7 @@ export class Pencil {
   pencilDegregation(text) {
     for (let letter of text) {
 
-      if(letter == "\n" || letter == " ") {
+      if(this.isNewlineOrSpace(letter)) {
         this.durability = this.durability
       } else if (this.isUppercaseLetter(letter)) {
         this.durability -= 2
@@ -23,6 +23,10 @@ export class Pencil {
       }
 
     }
+  }
+
+  isNewlineOrSpace(letter) {
+    return letter == "\n" || letter == " "
   }
 
   isUppercaseLetter(letter) {
